390a86952bd6b91e27c8d29b084f00a6221ac9ac
xu <231244454.@qq.com>
2019-08-16 15:15:14 +0800

    0

---
+++ porvider_ztx/src/main/java/com/jk/PorviderZtxApplication.java
@@ -0,0 +1,13 @@
+package com.jk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PorviderZtxApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PorviderZtxApplication.class, args);
+    }
+
+}
